9e5ddfb327c7055692a985f1b19e1ea95ef4142c
peter0715 <peterpeterpeter0715@gmail.com>
2019-12-05 10:21:29 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-
+import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
+
+
 while(True):
   # Capture frame-by-frame
   ret, frame = cap.read()
-
+  gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
   # Our operations on the frame come here
-  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  cutoff = cv2.inRange(hsv, (70, 50, 50), (80, 256, 256))
+ 
+  blur = cv2.GaussianBlur(gray, (5,5), 0)
+  ret, bw = cv2.threshold(blur,127,255,cv2.THRESH_BINARY)
+  canny = cv2.Canny(bw,100,200)
+  contours, hierarchy = cv2.findContours(bw,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+  img = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
 
-  # Display the resulting frame
-  cv2.imshow('frame', cutoff)
+
+  cv2.imshow('gray', bw)
+  cv2.imshow('edge', canny)
+  cv2.imshow('ede', img)
+
+  cv2.imshow('frame', frame)
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
