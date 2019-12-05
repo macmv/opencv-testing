e5752bcd134bd90f19b8403038e532a9d50c0733
Nathan Hale Robotics Laptop <nhalerobotics@gmail.com>
2019-12-05 10:40:22 -0800

    Finding rotated rectangles from contours

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
 while(True):
-  # Capture frame-by-frame
   ret, frame = cap.read()
 
-  # Our operations on the frame come here
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  cutoff = cv2.inRange(hsv, (70, 50, 50), (80, 256, 256))
+  cutoff = cv2.inRange(hsv, (70, 100, 100), (80, 255, 255))
+  blur = cv2.GaussianBlur(cutoff, (11, 11), 0)
 
-  # Display the resulting frame
-  cv2.imshow('frame', cutoff)
+  contours, h = cv2.findContours(blur, 1, 2)
+
+  for c in contours:
+    rect = cv2.minAreaRect(c)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(frame, [box], 0, (0,255,0), 2)
+
+  cv2.imshow('frame', frame)
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
 
