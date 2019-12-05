7529fc5806ece95afc0a5716f0e7fb335be3d159
peter0715 <peterpeterpeter0715@gmail.com>
2019-12-05 10:38:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ while(True):
   ret, frame = cap.read()
   gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
   # Our operations on the frame come here
- 
-  blur = cv2.GaussianBlur(gray, (5,5), 0)
+  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+  blue = cv2.inRange(hsv,(80,100,100),(110,255,255))
+  blur = cv2.GaussianBlur(blue, (5,5), 0)
   ret, bw = cv2.threshold(blur,127,255,cv2.THRESH_BINARY)
   canny = cv2.Canny(bw,100,200)
   contours, hierarchy = cv2.findContours(bw,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
   img = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
-
-
   cv2.imshow('gray', bw)
   cv2.imshow('edge', canny)
   cv2.imshow('ede', img)
-
+  cv2.imshow('blue',blue)
   cv2.imshow('frame', frame)
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
