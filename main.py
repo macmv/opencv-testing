35331c4d12c226039522fb995f61a383bfe4402d
peter0715 <peterpeterpeter0715@gmail.com>
2019-12-11 14:10:08 -0800

    show only the biggest detection

--- main.py
+++ main.py
@@ -8,22 +8,43 @@ cap = cv2.VideoCapture(0)
 while(True):
   # Capture frame-by-frame
   ret, frame = cap.read()
-  gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+  #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+  #ret, thres = cv2.threshold(gray, 127,255,cv2.THRESH_BINARY)
   # Our operations on the frame come here
   # this code is for canny edging the blue umbrella I have
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  blue = cv2.inRange(hsv,(80,100,100),(110,255,255))
+  blue = cv2.inRange(hsv,(90,50,50),(130,255,255))
   blur = cv2.GaussianBlur(blue, (5,5), 0)
-  ret, bw = cv2.threshold(blur,127,255,cv2.THRESH_BINARY)
-  canny = cv2.Canny(bw,100,200)
-  contours, hierarchy = cv2.findContours(bw,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-  img = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
-  cv2.imshow('gray', bw)
-  cv2.imshow('edge', canny)
-  cv2.imshow('ede', img)
-  cv2.imshow('blue',blue)
-  cv2.imshow('frame', frame)
-
+  ret, thres = cv2.threshold(blur,127,255,cv2.THRESH_BINARY)
+  canny = cv2.Canny(thres,100,200)
+  contours, hierarchy = cv2.findContours(thres,1,2)
+  # create hull array for convex hull points
+  hull = []
+  # calculate points for each contour
+  for i in range(len(contours)):
+    # creating convex hull object for each contour
+    hull.append(cv2.convexHull(contours[i], False))
+  # create an empty black image
+  drawing = np.zeros((canny.shape[0], canny.shape[1], 3), np.uint8)
+  wMax = 0
+  hMax = 0
+# draw contours and hull points
+  for i in range(len(contours)):
+    
+    
+    color_contours = (0, 255, 0) # green - color for contours
+    color = (255, 0, 0) # blue - color for convex hull
+    # draw ith contour
+    cv2.drawContours(canny, contours, i, color_contours, 1, 8, hierarchy)
+    # draw ith convex hull object
+    x,y,w,h = cv2.boundingRect(hull[i])
+    if w > wMax:
+      wMax = w
+    if h > hMax:
+      hMax = h
+  img = cv2.rectangle(drawing,(x,y),(x+wMax,y+hMax),(0,255,0),2)
+  cv2.imshow('frame', canny)
+  cv2.imshow('',img)
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
 
