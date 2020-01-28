e226c26622d4da3bf97a2b7ed0edbf8f2bd32375
peter0715 <peterpeterpeter0715@gmail.com>
2020-01-28 11:34:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 #this code will only work when the paper is parrell to the camera lens, 
 #so I still need to work on it
+#It shows the vertical distance 
+#and horizontal shift of the camera and the paper
 
 import cv2
 import numpy as np
 
-# turn on camera at port 0
+# turn on camera at port 1
 cap = cv2.VideoCapture(1)
-# width of the paper
-KNOWN_WIDTH = 11.0
-#the focallength of the camera
+# width of the paper in inches
+KNOWN_WIDTH = 5.5
+
+#the focallength of the camera will need to be modified
 #It is currently defaulted to my camera
 #this number will vary due to different camera modules 
+
+#laptop focallength 
 #focalLength = 800
 #webCam focallength
 focalLength = 1200
-#vertical distance
-distanceV = 0
-#horizontal Shift
-didtanceH = 0
+
 
 
 def distance_to_camera(knownWidth, focalLength, perWidth):
@@ -39,7 +41,6 @@ while(True):
 	# find all contours
 	max_area = -1
 	max_box = None
-	max_cnt = None
 	# loop through contours
 	for c in contours:
 		# rect is ((x, y), (w, h), rot)
@@ -54,29 +55,38 @@ while(True):
 		if area > max_area:
 			max_area = area
 			max_box = box
-			max_cnt = rect
 
+	#get the max countor detail
 	x,y,w,h	= cv2.boundingRect(max_box)
+	#get image width
 	imageW = frame.shape[1]
-  
-	# only draw the contour and calculate distance of the max_area has been set
+  	#vertical distance
+	distanceV = 0
+	#horizontal Shift
+	distanceH = 0
+	# only draw the contour and calculate distance once the max_area has been set
 	if max_area > -1:
 		cv2.drawContours(frame, [max_box], 0, (0,255,0), 2)
+		#this is the instant distance between paper and camera
 		distanceV = distance_to_camera(KNOWN_WIDTH, focalLength, w)
+		#default means when camera is at the origin of the paper
 		default_W = (KNOWN_WIDTH * focalLength) / distanceV
 		default_Shift = (imageW / 2) - (default_W / 2)
+		#this is the instant distance of horizontal shift
 		distanceH = (default_Shift - x) / focalLength * distanceV
     
 
-  #show distance
+    #show distance
 	cv2.putText(frame, "%.2fft" % (distanceV / 12),
 		(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
 		2.0, (0, 255, 0), 3)
-	
+	cv2.putText(frame, "%.2fin" % distanceH,
+		(frame.shape[1] - 200, frame.shape[0] - 80), cv2.FONT_HERSHEY_SIMPLEX,
+		2.0, (0, 255, 0), 3)
 
 	# draw frame to screen
 	cv2.imshow('frame', frame)
-
+	cv2.imshow("edge", edged)
 	# if q is pressed, quit
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
