95ba1b7ed4b7047ab4bf9adf5b65bdb7c6f5acb5
peter0715 <peterpeterpeter0715@gmail.com>
2020-01-28 01:31:32 -0800

    calculate distance from a paper

--- main.py
+++ main.py
@@ -1,57 +1,79 @@
-import numpy as np
+#this code will only work when the paper is parrell to the camera lens, 
+#so I still need to work on it
+
 import cv2
+import numpy as np
 
+# turn on camera at port 0
 cap = cv2.VideoCapture(0)
+# width of the paper
+KNOWN_WIDTH = 11.0
+#the focallength of the camera
+#It is currently defaulted to my camera
+#this number will vary due to different camera modules 
+focalLength = 800
+#vertical distance
+distanceV = 0
 
 
+def distance_to_camera(knownWidth, focalLength, perWidth):
+	# compute and return the distance from the maker to the camera
+	return (knownWidth * focalLength) / perWidth
 
 while(True):
-  # Capture frame-by-frame
-  ret, frame1 = cap.read()
-  frame = cv2.flip(frame1,1)
-  #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-  #ret, thres = cv2.threshold(gray, 127,255,cv2.THRESH_BINARY)
-  # Our operations on the frame come here
-  # this code is for canny edging the blue umbrella I have
-  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  blue = cv2.inRange(hsv,(90,50,50),(130,255,255))
-  blur = cv2.GaussianBlur(blue, (5,5), 0)
-  ret, thres = cv2.threshold(blur,127,255,cv2.THRESH_BINARY)
-  canny = cv2.Canny(thres,100,200)
-  contours, hierarchy = cv2.findContours(thres,1,2)
-  # create hull array for convex hull points
-  hull = []
-  # calculate points for each contour
-  for i in range(len(contours)):
-    # creating convex hull object for each contour
-    hull.append(cv2.convexHull(contours[i], False))
-  # create an empty black image
-  drawing = np.zeros((canny.shape[0], canny.shape[1], 3), np.uint8)
-  wMax = 0
-  hMax = 0
-# draw contours and hull points
-  for i in range(len(contours)):
-    
-    
-    color_contours = (0, 255, 0) # green - color for contours
-    color = (255, 0, 0) # blue - color for convex hull
-    # draw ith contour
-    cv2.drawContours(canny, contours, i, color_contours, 1, 8, hierarchy)
-    # draw ith convex hull object
-    x,y,w,h = cv2.boundingRect(hull[i])
-    if w > wMax:
-      wMax = w
-    if h > hMax:
-      hMax = h
-  img = cv2.rectangle(frame,(x,y),(x+wMax,y+hMax),(0,255,0),2)
-  canny = cv2.rectangle(canny,(x,y),(x+wMax,y+hMax),(0,255,0),2)
+	# read frame from camera
+	ret, frame = cap.read()
+  #grayscale it and blur it and edge it
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 35, 125)
 
-  cv2.imshow('frame', canny)
-  cv2.imshow('',img)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+	# find the contours in the edged frame and keep the largest one;
+	# we'll assume that this is our piece of paper in the frame
+	contours, h = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-# When everything done, release the capture
+	# find all contours
+	max_area = -1
+	max_box = None
+	max_cnt = None
+	# loop through contours
+	for c in contours:
+		# rect is ((x, y), (w, h), rot)
+		rect = cv2.minAreaRect(c)
+		# gets the four points on the outside of the rectangle
+		box = cv2.boxPoints(rect)
+		# convert to int
+		box = np.int0(box)
+		# find area of contour
+		area = cv2.contourArea(box)
+		# if area is greater than max, set the max area and contour
+		if area > max_area:
+			max_area = area
+			max_box = box
+			max_cnt = rect
+
+	x,y,w,h	= cv2.boundingRect(max_box)
+
+  
+	# only draw the contour and calculate distance of the max_area has been set
+	if max_area > -1:
+		cv2.drawContours(frame, [max_box], 0, (0,255,0), 2)
+		distanceV = distance_to_camera(KNOWN_WIDTH, focalLength, w)
+    
+
+  #show distance
+	cv2.putText(frame, "%.2fft" % (distanceV / 12),
+		(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+		2.0, (0, 255, 0), 3)
+	
+
+	# draw frame to screen
+	cv2.imshow('frame', frame)
+
+	# if q is pressed, quit
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# close windows
 cap.release()
 cv2.destroyAllWindows()
-
